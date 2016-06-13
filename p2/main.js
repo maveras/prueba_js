5473b6513c92d8c94b8d20b17325311c3b177e97
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-06-13 20:56:59 -0300

    p3 base

--- p2/main.js
+++ p2/main.js
@@ -1,3 +1,11 @@
+
 var copia_objeto = function (obj){
 	
+	var copia = obj.slice();
+	return copia;
+
+	/*for (var valor in obj){
+		
+    	console.log(obj[valor]);
+	}*/
 }
---
+++ p3/main.js
@@ -0,0 +1,15 @@
+
+var alumno = function (nombre,notas){
+	this.nombre = nombre,
+	this.notas = notas
+
+	if(!is_string(this.nombre)){
+		console.log("El nombre ingresado no es una cadena");
+	}
+
+	/*this.promedio = (notas){
+
+	}*/
+	
+	
+}
