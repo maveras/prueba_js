5d17619acc43f71b80690260a63e6ce5af228cf4
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-06-13 22:26:13 -0300

    p4 ok

--- p4/main.js
+++ p4/main.js
@@ -1,36 +1,11 @@
+$( document ).ready(function() {
+	$("#boton").click(function(){
+		var texto = $("#texto").val();
+		var color = $("#color").val()
+		var tam_letra = $("#tam_letra").val();
+		var tipografia = $("#tipografia").val();
+		
 
-var alumno = function (nombre,notas){
-	this.nombre = nombre,
-	this.notas = notas
-
-	var tipo_nombre = typeof(this.nombre);
-	var tipo_notas = notas instanceof Array;
-	var suma = 0;
-	var promedio = 0;
-
-	 if (!(tipo_nombre == "string")){
-		console.log("El nombre ingresado no es una cadena");
-		return false;
-	} else {
-		//console.log("Es cadena");
-		if(tipo_notas){
-			this.notas =  notas.filter(function(e){
-		    	return !isNaN(e)
-		    	}).map(function(e){
-		    		return Math.round(e)
-		    	})
-		} else {
-		    alert("el valor de notas no es arreglo")
-		    return false;
-		}
-			
-	}
-
-	///metodo
-	this.calcula_promedio = function () {
-	  for (var i=0; i<this.notas.length; i++){
-	  	suma = suma + parseInt(this.notas[i]);
-	  }
-	  	return promedio = suma / this.notas.length;
-	};
-}
+		$("#derecha").html("<p style='color:"+color+";font-size:"+tam_letra+"px; font-family: "+tipografia+"   '    >"+texto+"</p>");
+	});
+});
