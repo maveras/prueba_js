b13ff6714843d44bdeb6280238aba62a077c6583
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-06-13 22:59:24 -0300

    p5 ok

--- p5/main.js
+++ p5/main.js
@@ -9,3 +9,31 @@ $( document ).ready(function() {
 		$("#derecha").html("<p style='color:"+color+";font-size:"+tam_letra+"px; font-family: "+tipografia+"   '    >"+texto+"</p>");
 	});
 });
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        
+        var actual = $(".cuadrado").position().left;
+        var nuevo = actual - 50; 
+        $(".cuadrado").css("left", ""+nuevo+"px");
+        
+        break;
+
+        
+
+        case 39: // right
+        
+        var actual = $(".cuadrado").position().left;
+        var nuevo = actual + 50; 
+        
+        $(".cuadrado").css("left", ""+nuevo+"px");
+        
+        break;
+
+        
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
